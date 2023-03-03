18ceb6491421c416ce1f8b36b22d788a19b59fef
Louis  Cooper <126891713+cooperl88@users.noreply.github.com>
2023-03-03 15:50:06 +0000

    Created Date.java. Added lines to display variables as well as displays American and European date formats.

--- pre02/Date.java
+++ pre02/Date.java
@@ -1,8 +1,20 @@
-import java.io.*;
+import java.io.*; //boilerplate, imports java utils
 import java.util.*;
 
-public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
-    }
+public class Date{
+  public static void main(String[] args){
+    String day = "Monday";
+    String month = "June";
+    int year = 2020;
+    int date = 15;
+    //Display variables and American / European Date Formats
+    System.out.print("The year is: " + year + ".\n");
+    System.out.print("The month is: " + month + ".\n");
+    System.out.print("The day of the week is: " + day + ".\n");
+    System.out.print("The day of the month is the: " + date + "th" + ".\n");
+    System.out.print("American Date Format: " + day + ", " + month + " " + date + "," + " " + year + "\n");
+    System.out.print("European Date Format: " + day + ", " + date + " " + month + "," + " " + year);
+  }
 }
+
+
