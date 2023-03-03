1e9d86727830e0218dfcdd833c475ff31c343cde
Louis  Cooper <126891713+cooperl88@users.noreply.github.com>
2023-03-03 19:22:08 +0000

    Started to work on Celsius program. Added equation to convert C to F. Started code to ask for input on screen.

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 public class Celsius{
   public static void main(String[] args){
-    
+   double celsius; 
+   double celsiusToFarenheit = celsius * (9/5 + 32);
+
+
+
+    /**System.out.println("Hello world!");
+
+    Scanner in = new Scanner(System.in);
+    String celsius;
+    System.out.print("Type something: ");
+    celsius = in.nextLine();
+    System.out.printf("%f", celsius);
+    **/
   }
 }
