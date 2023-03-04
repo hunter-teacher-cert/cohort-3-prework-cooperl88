2f0acc2f1be3d2d3a174b7fd794ae9ce94737589
Louis  Cooper <126891713+cooperl88@users.noreply.github.com>
2023-03-04 20:02:35 +0000

    Committed Guess.java, added import section, started Guess.Java lines 5-6

--- pre02/Guess.java
+++ pre02/Guess.java
@@ -3,5 +3,6 @@ import java.util.*;
 
 public class Date{
     System.out.print("Welcome to the number guessing game!");
+System.out.print("I'm thinking of a number between 1 and 100\n (including both). Can you guess what it is?");
   }
 }
