fcffde548d7dd4e648765cba944ffb72cc1dd676
Louis  Cooper <126891713+cooperl88@users.noreply.github.com>
2023-03-05 22:21:52 +0000

    Added and completed answer.txt. Also created an example program with the code for reference.

---
+++ pre03/Example.java
@@ -0,0 +1,27 @@
+import java.io.*; //boilerplate, imports java utils
+import java.util.*;
+import java.util.Scanner;
+
+public class Example{
+  public static void zoop() {
+    baffle();
+    System.out.print("You wugga ");
+    baffle();
+}
+
+public static void main(String[] args) {
+    System.out.print("No, I ");
+    zoop();
+    System.out.print("I ");
+    baffle();
+}
+
+public static void baffle() {
+    System.out.print("wug");
+    ping();
+}
+
+public static void ping() {
+    System.out.println(".");
+}
+}
