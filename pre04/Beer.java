743f81dd8724cd005fec04ece0f05aa56f49d309
Louis  Cooper <126891713+cooperl88@users.noreply.github.com>
2023-03-06 19:56:16 +0000

    Completed Beer.java

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -2,19 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Beer{
-
-  public static void song(string verse){
-    System.out.println("x bottle of beer on the wall");
-    System.out.println("x bottles of beer");
-    System.out.println("Take on down, pass it around");
-    System.out.println("x bottle of beer on the wall");
+  //prints beer song and subtracts 1 from numberOfBeers when finished
+  public static void song(int numberOfBeers){
+    System.out.println(numberOfBeers + " bottles of beer on the wall,");
+    System.out.println(numberOfBeers + " bottles of beer,");
+    System.out.println("Take on down, pass it around,");
+    System.out.println(numberOfBeers - 1 + " bottle of beer on the wall\n");
   }
+  //recursive method & conditionals to subtract beer amount and finish conditon
   public static void countDown(int beers){
     if(beers == 0){
-      System.out.println("NO beer left");
+      System.out.println("No bottles of beer on the wall,");
+      System.out.println("no bottles of beer,");
+      System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
+      System.out.println("’cause there are no more bottles of beer on the wall!");
     } else{
-      //countDown(beers - 1); //why is this increasing count not decreasing?
-      System.out.println(beers);
+      song(beers);          // calls the beer song
+      countDown(beers - 1); //decrements beers argument
     }
   }
   
